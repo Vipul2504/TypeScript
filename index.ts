b378bc882f4371e1823730540f1348f7a94f4fe1
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-11 18:15:50 +0530

    learning TypeScript

---
+++ index.ts
@@ -0,0 +1,31 @@
+/**
+ * This function prints all the car details to the console.
+ * @param make - This is the company that manufactured the car.
+ * @param model - The year of manufacture.
+ * @param sedan - Indicated whether the car is a sedan or not.
+ */
+
+type Car = {
+    model:number,
+    make:string,
+    sedan:boolean
+}
+function printCarDetails({ model, make, sedan }: Car) {
+    console.log("My Car model is " + model);
+    
+    let nextModel = model + 1;
+    console.log("My Car's next model will be " + nextModel);
+    console.log("My Car manufacturer is " + make.toUpperCase());
+
+    if (sedan) {
+        console.log("My Car is sedan");
+    } else {
+        console.log("My Car is not a sedan");
+    }
+}
+
+printCarDetails({
+    model: 2021,
+    make: 'bmw',
+    sedan: true
+})
